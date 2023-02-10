695aea32ed7b5cb77166de065701543f0c9784b5
Wouter Smaardijk <287574@edu.rocwb.nl>
2023-02-10 14:30:15 +0100

    Trying to set up the basic structure

--- TextBasedAdventure/Program.cs
+++ TextBasedAdventure/Program.cs
@@ -6,10 +6,57 @@ using System.Threading.Tasks;
 
 namespace TextBasedAdventure
 {
-    internal class Program
+    enum GameState
     {
-        static void Main(string[] args)
+        Start,
+        Choice1,
+        Choice2,
+        End
+    }
+
+    class Program
+    {
+        static void Main()
         {
+            GameState currentState = GameState.Start;
+
+            while (currentState != GameState.End)
+            {
+                switch (currentState)
+                {
+                    case GameState.Start:
+                        Console.WriteLine("Welcome to the game!");
+                        Console.WriteLine("Make a choice:");
+                        Console.WriteLine("1. Option 1");
+                        Console.WriteLine("2. Option 2");
+                        string choice = Console.ReadLine();
+
+                        if (choice == "1")
+                        {
+                            currentState = GameState.Choice1;
+                        }
+                        else if (choice == "2")
+                        {
+                            currentState = GameState.Choice2;
+                        }
+                        break;
+
+                    case GameState.Choice1:
+                        Console.WriteLine("You chose Option 1.");
+                        currentState = GameState.Choice1;
+                        break;
+
+                    case GameState.Choice2:
+                        Console.WriteLine("You chose Option 2.");
+                        currentState = GameState.End;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing!");
+            Console.ReadKey();
         }
     }
+
 }
+

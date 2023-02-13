0c0f5f85cc1b201e52426649107fd57884ff49bd
Wouter Smaardijk <287574@edu.rocwb.nl>
2023-02-13 10:19:33 +0100

    beetje aangepast

--- TextBasedAdventure/Program.cs
+++ TextBasedAdventure/Program.cs
@@ -14,7 +14,8 @@ namespace TextBasedAdventure
         }
         private static void MainMenu()
         {
-            Console.WriteLine("Main Menu\nPlease select:\n1. Start Game\n2. Exit\n>");
+            Console.WriteLine("Main Menu\nPlease select:\n1. Start Game\n2. Exit");
+            Console.Write(">");
 
             string answer = Console.ReadLine();
             switch (answer)
@@ -42,33 +43,33 @@ namespace TextBasedAdventure
             Console.WriteLine("2. Go through door 2");
             Console.WriteLine("3. Look at the map");
             Console.WriteLine("4. Open the chest");
-            Console.Write(">");
-            
-            int choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
+            string choice = "";
+            while (choice != "1" || choice != "2" || choice != "3" || choice != "4")
             {
-                case 1:
-                    RoomOne();
-                    break;
-                case 2:
-                    RoomTwo();
-                    break;
-                case 3:
-                    LibraryMap();
-                    break;
-                case 4:
-                    LibraryChest();
-                    break;
-                default:
-                    RoomOne();
-                    break;
-
+                Console.WriteLine("Please enter a valid choice");
+                Console.Write(">");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        RoomOne();
+                        break;
+                    case "2":
+                        RoomTwo();
+                        break;
+                    case "3":
+                        LibraryMap();
+                        break;
+                    case "4":
+                        LibraryChest();
+                        break;
+                }
             }
         }
 
         private static void RoomOne()
         {
+    
 
         }
         private static void RoomTwo()
